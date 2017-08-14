ecc401eb36d2ee65e6a1a195abc83d99932be986
Kevin Wood <kwood@mesosphere.com>
2017-08-14 12:48:40 -0500

    removed import * and isort

--- frameworks/kafka/tests/test_utils.py
+++ frameworks/kafka/tests/test_utils.py
@@ -1,18 +1,10 @@
 import json
 import logging
 
-import shakedown
 import sdk_cmd
 import sdk_tasks
-
-from tests.config import (
-    PACKAGE_NAME,
-    SERVICE_NAME,
-    DEFAULT_PARTITION_COUNT,
-    DEFAULT_BROKER_COUNT,
-    DEFAULT_POD_TYPE,
-    DEFAULT_TASK_NAME
-)
+import shakedown
+from tests import config
 
 log = logging.getLogger(__name__)
 
@@ -20,8 +12,8 @@ DEFAULT_TOPIC_NAME = 'topic1'
 EPHEMERAL_TOPIC_NAME = 'topic_2'
 
 
-def service_cli(cmd_str, get_json=True, print_output=True, service_name=SERVICE_NAME):
-    full_cmd = '{} --name={} {}'.format(PACKAGE_NAME, service_name, cmd_str)
+def service_cli(cmd_str, get_json=True, print_output=True, service_name=config.SERVICE_NAME):
+    full_cmd = '{} --name={} {}'.format(config.PACKAGE_NAME, config.SERVICE_NAME, cmd_str)
     ret_str = sdk_cmd.run_cli(full_cmd, print_output=print_output)
     if get_json:
         return json.loads(ret_str)
@@ -29,10 +21,10 @@ def service_cli(cmd_str, get_json=True, print_output=True, service_name=SERVICE_
         return ret_str
 
 
-def broker_count_check(count, service_name=SERVICE_NAME):
+def broker_count_check(count, service_name=config.SERVICE_NAME):
     def fun():
         try:
-            if len(service_cli('broker list', service_name=service_name)) == count:
+            if len(service_cli('broker list', service_name=config.SERVICE_NAME)) == count:
                 return True
         except:
             pass
@@ -41,44 +33,44 @@ def broker_count_check(count, service_name=SERVICE_NAME):
     shakedown.wait_for(fun)
 
 
-def restart_broker_pods(service_name=SERVICE_NAME):
-    for i in range(DEFAULT_BROKER_COUNT):
-        broker_id = sdk_tasks.get_task_ids(service_name,'{}-{}-{}'.format(DEFAULT_POD_TYPE, i, DEFAULT_TASK_NAME))
-        restart_info = service_cli('pod restart {}-{}'.format(DEFAULT_POD_TYPE, i), service_name=service_name)
-        sdk_tasks.check_tasks_updated(service_name, '{}-{}-{}'.format(DEFAULT_POD_TYPE, i, DEFAULT_TASK_NAME), broker_id)
-        sdk_tasks.check_running(service_name, DEFAULT_BROKER_COUNT)
+def restart_broker_pods(service_name=config.SERVICE_NAME):
+    for i in range(config.DEFAULT_BROKER_COUNT):
+        broker_id = sdk_tasks.get_task_ids(config.SERVICE_NAME,'{}-{}-{}'.format(config.DEFAULT_POD_TYPE, i, config.DEFAULT_TASK_NAME))
+        restart_info = service_cli('pod restart {}-{}'.format(config.DEFAULT_POD_TYPE, i), service_name=config.SERVICE_NAME)
+        sdk_tasks.check_tasks_updated(config.SERVICE_NAME, '{}-{}-{}'.format(config.DEFAULT_POD_TYPE, i, config.DEFAULT_TASK_NAME), broker_id)
+        sdk_tasks.check_running(config.SERVICE_NAME, config.DEFAULT_BROKER_COUNT)
         assert len(restart_info) == 2
-        assert restart_info['tasks'][0] == '{}-{}-{}'.format(DEFAULT_POD_TYPE, i, DEFAULT_TASK_NAME)
+        assert restart_info['tasks'][0] == '{}-{}-{}'.format(config.DEFAULT_POD_TYPE, i, config.DEFAULT_TASK_NAME)
 
 
-def replace_broker_pod(service_name=SERVICE_NAME):
-    broker_0_id = sdk_tasks.get_task_ids(service_name, '{}-0-{}'.format(DEFAULT_POD_TYPE, DEFAULT_TASK_NAME))
-    service_cli('pod replace {}-0'.format(DEFAULT_POD_TYPE), service_name=service_name)
-    sdk_tasks.check_tasks_updated(service_name, '{}-0-{}'.format(DEFAULT_POD_TYPE, DEFAULT_TASK_NAME), broker_0_id)
-    sdk_tasks.check_running(service_name, DEFAULT_BROKER_COUNT)
+def replace_broker_pod(service_name=config.SERVICE_NAME):
+    broker_0_id = sdk_tasks.get_task_ids(config.SERVICE_NAME, '{}-0-{}'.format(config.DEFAULT_POD_TYPE, config.DEFAULT_TASK_NAME))
+    service_cli('pod replace {}-0'.format(config.DEFAULT_POD_TYPE), service_name=config.SERVICE_NAME)
+    sdk_tasks.check_tasks_updated(config.SERVICE_NAME, '{}-0-{}'.format(config.DEFAULT_POD_TYPE, config.DEFAULT_TASK_NAME), broker_0_id)
+    sdk_tasks.check_running(config.SERVICE_NAME, config.DEFAULT_BROKER_COUNT)
     # wait till all brokers register
-    broker_count_check(DEFAULT_BROKER_COUNT, service_name=service_name)
+    broker_count_check(config.DEFAULT_BROKER_COUNT, service_name=config.SERVICE_NAME)
 
 
-def create_topic(service_name=SERVICE_NAME):
-    create_info = service_cli('topic create {}'.format(EPHEMERAL_TOPIC_NAME), service_name=service_name)
+def create_topic(service_name=config.SERVICE_NAME):
+    create_info = service_cli('topic create {}'.format(EPHEMERAL_TOPIC_NAME), service_name=config.SERVICE_NAME)
     log.info(create_info)
     assert ('Created topic "%s".\n' % EPHEMERAL_TOPIC_NAME in create_info['message'])
     assert ("topics with a period ('.') or underscore ('_') could collide." in create_info['message'])
-    topic_list_info = service_cli('topic list', service_name=service_name)
+    topic_list_info = service_cli('topic list', service_name=config.SERVICE_NAME)
     assert topic_list_info == [EPHEMERAL_TOPIC_NAME]
 
-    topic_info = service_cli('topic describe {}'.format(EPHEMERAL_TOPIC_NAME), service_name=service_name)
+    topic_info = service_cli('topic describe {}'.format(EPHEMERAL_TOPIC_NAME), service_name=config.SERVICE_NAME)
     assert len(topic_info) == 1
-    assert len(topic_info['partitions']) == DEFAULT_PARTITION_COUNT
+    assert len(topic_info['partitions']) == config.DEFAULT_PARTITION_COUNT
 
 
-def delete_topic(service_name=SERVICE_NAME):
-    delete_info = service_cli('topic delete {}'.format(EPHEMERAL_TOPIC_NAME), service_name=service_name)
+def delete_topic(service_name=config.SERVICE_NAME):
+    delete_info = service_cli('topic delete {}'.format(EPHEMERAL_TOPIC_NAME), service_name=config.SERVICE_NAME)
 
     assert len(delete_info) == 1
     assert delete_info['message'].startswith('Output: Topic {} is marked for deletion'.format(EPHEMERAL_TOPIC_NAME))
 
-    topic_info = service_cli('topic describe {}'.format(EPHEMERAL_TOPIC_NAME), service_name=service_name)
+    topic_info = service_cli('topic describe {}'.format(EPHEMERAL_TOPIC_NAME), service_name=config.SERVICE_NAME)
     assert len(topic_info) == 1
-    assert len(topic_info['partitions']) == DEFAULT_PARTITION_COUNT
+    assert len(topic_info['partitions']) == config.DEFAULT_PARTITION_COUNT
